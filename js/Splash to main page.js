9439872f3e197c7a05fa5398ddacb02f27a35c57
ray54-boom <113106425+ray54-boom@users.noreply.github.com>
2023-11-08 00:20:07 +0700

    Add files via upload

---
+++ js/Splash to main page.js
@@ -0,0 +1,5 @@
+    document.getElementById('splash-screen').addEventListener('click', function() {
+        this.style.display = 'none';
+        document.getElementById('main-page').style.display = 'block';
+    });
+    
